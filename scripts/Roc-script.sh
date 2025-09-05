7dc954fb93e8a4035be201662e5e3d523ba6049c
yjiawqgj <71002956+yjiawqgj@users.noreply.github.com>
2025-09-05 11:14:25 +0800

    Update Roc-script.sh

--- scripts/Roc-script.sh
+++ scripts/Roc-script.sh
@@ -46,5 +46,8 @@ git clone --depth=1 https://github.com/lwb1978/openwrt-gecoosac package/openwrt-
 git clone --depth=1 https://github.com/NONGFAH/luci-app-athena-led package/luci-app-athena-led
 chmod +x package/luci-app-athena-led/root/etc/init.d/athena_led package/luci-app-athena-led/root/usr/sbin/athena-led
 
+#添加
+git clone --depth=1 https://github.com/sirpdboy/luci-app-ddns-go package/luci-app-ddns-go
+
 ./scripts/feeds update -a
 ./scripts/feeds install -a
